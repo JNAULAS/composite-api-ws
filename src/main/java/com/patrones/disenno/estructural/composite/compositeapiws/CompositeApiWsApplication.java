98898ce7573f92aceb4e661e1f2832f29393b764
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-09-25 16:25:15 -0500

    Se agrega proyecto base

---
+++ src/main/java/com/patrones/disenno/estructural/composite/compositeapiws/CompositeApiWsApplication.java
@@ -0,0 +1,13 @@
+package com.patrones.disenno.estructural.composite.compositeapiws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeApiWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeApiWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrones/disenno/estructural/composite/compositeapiws/CompositeApiWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.disenno.estructural.composite.compositeapiws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeApiWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
