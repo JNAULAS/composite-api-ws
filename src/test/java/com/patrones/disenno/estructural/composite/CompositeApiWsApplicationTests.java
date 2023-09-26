45384c49400716fa6eff6db73380948f2961a923
jnaulas <juangns_sudamericano@yahoo.es>
2023-09-25 19:27:02 -0500

    se agrega nuevo proyecto base

---
+++ src/test/java/com/patrones/disenno/estructural/composite/CompositeApiWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.disenno.estructural.composite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeApiWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
